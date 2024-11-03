3c1f6762342160b2e98d8a601171236dfe9f61cb
Devin Rider <drider539@gmail.com>
2024-11-03 15:15:00 -0800

    JavaScript Basics

---
+++ script.js
@@ -0,0 +1,11 @@
+// Task 1 + 2
+studentName = "Devin";
+studentAge = 16;
+studentGrade = 11;
+isPresent = true;
+
+// Task 3
+console.log('Student Name:', studentName);
+console.log('Age:', studentAge);
+console.log('Grade Level:', studentGrade);
+console.log('Was he present?', isPresent);
---
+++ script2.js
@@ -0,0 +1,27 @@
+// Task 1 + 2
+let x = 12;
+let y = 24;
+
+// Task 3
+console.log("Sum:", x + y);
+console.log("Difference:", x - y);
+console.log("Product:", x * y);
+console.log("Quotient:", x / y);
+
+// Task 4
+x = 10;
+y = 14;
+
+console.log("Updated num1:", x);
+console.log("Updated num2:", y);
+
+// Task 5
+console.log("Is num1 equal to num2?", x == y);
+console.log("Is num1 greater than num2?", x > y);
+console.log("Is num1 not equal to num2?", x != y);
+
+// Task 6
+let isPositive = x > 0 && y > 0;
+let isEven = x % 2 === 0 || y % 2 === 0;
+console.log("Are both numbers positive?", isPositive);
+console.log("Is at least one number even?", isEven);
